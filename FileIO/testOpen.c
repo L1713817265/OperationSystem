17746e9e0d8d6a3109aeef7dbc2e3317da11b3c3
L1713817265 <1713817265@qq.com>
2024-01-04 16:10:21 +0800

    [新增] 打开文件失败

---
+++ FileIO/testOpen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int main()
+{
+    int fd = open("./hello.txt", O_RDWR, 0644);
+    if(fd == -1)
+    {
+        perror("open error");
+    }
+
+    return 0;
+}
