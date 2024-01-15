e9896be43c4244ef5193eb432ce66098eba534e6
L1713817265 <1713817265@qq.com>
2024-01-15 22:06:43 +0800

    新增]使用access函数实现判断文件是否存在

---
+++ FileIO/testAccess.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* F_OK 判断文件是否存在 */
+    int ret = access("./heiheihei.c", F_OK);
+    if(ret)
+    {
+        perror("access error");
+    }
+
+    return 0;
+}
